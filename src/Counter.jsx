f5bf60f9f537849566f71f3d1f8ef93b9e89987c
tristangirault <tristan.girault@kubity.com>
2018-02-12 17:03:26 +0100

    - changed the NumberDisplay function's call to be more comprehensive for me (on a single line)

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -54,10 +54,7 @@ export default class Counter extends React.Component {
 		
 		return(
 			<div>
-				<NumberDisplay
-					numberToDisplay={this.state.nb}
-					textColor={this.state.color}
-				/>
+				<NumberDisplay numberToDisplay={this.state.nb} textColor={this.state.color}/>
 				<div className = "buttonPanel" style = {buttonPanelStyle}>
 					<button onClick={ ( ) => this.increment() } style = {buttonStyle}>
 						+1
